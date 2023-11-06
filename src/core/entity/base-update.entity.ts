97b547e68a795ce69fc321ef60675ddb27e27fa9
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-11-07 03:33:00 +0900

    fix: updatedAt 필드 수동 업데이트로 변경 #69

--- src/core/entity/base-update.entity.ts
+++ src/core/entity/base-update.entity.ts
@@ -1,8 +1,8 @@
-import { UpdateDateColumn } from 'typeorm';
+import { Column } from 'typeorm';
 import { BaseEntity } from './base.entity';
 
 export class BaseUpdateEntity<T> extends BaseEntity<T> {
-  @UpdateDateColumn({
+  @Column({
     name: 'updated_at',
   })
   updatedAt?: Date;
